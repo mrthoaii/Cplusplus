024524d3932e5604d4d8f45bdf8dd4d3fda72c82
mrthoaii <phamquocthoai7a4@gmail.com>
2022-07-29 09:59:54 +0700

    Bai tap 07

---
+++ Cplusplus/Object_Oriented/baitap2707.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+/*
+1. Write a program to print the area and perimeter of a triangle having sides of 3, 4 
+and 5units by creating a class named 'Triangle' without any parameter in its constructor.
+*/
+class Triangle{
+    private:
+        float a,b,c;
+    public:
+        Triangle(){
+            cout<<"\nInput value of a b c :";
+            cin>>a>>b>>c;
+        }
+        ~Triangle(){
+        }
+        float Perimeter(){
+            return (a+b+c);
+        }
+        float Area (){
+            return sqrt((a+b+c)*(a-b+c)*(a+b-c)*(b+c-a))/4;
+        }
+};
+/*
+2. Print the average of three numbers entered by user by creating a class named 'Average'having 
+a method to calculate and print the average
+*/
+class Average{
+    private :
+        float a,b,c;
+        float result ;
+    public:
+        Average(float x, float y, float z){
+            a=x;
+            b=y;
+            c=z;
+        }
+        
+        void Calculator(){
+            result=(a+b+c)/3;
+        }
+        void print_result(){
+            cout<<"\nResult of Average :"<<result;
+        }
+};
+/*
+3. Print the  sum,  difference  and   product of   two  
+complex numbers by  creating a   classnamed 'Complex' with separate methods for each operation
+ whose real and imaginaryparts are entered by user.
+*/
+class Complex_Number{
+    private :
+        int real , image;
+    public:
+        Complex_Number(int a, int b){
+            real =a;
+            image =b;
+        }
+        void print_complex(){
+            cout<<real<<" + "<<image<<"i";
+        }
+};
+/*
+Write a program by creating an 'Employee' class having the following methods and printthe final salary.(15$/day)
+ 1 - 'getInfo()' which takes the salary, number of hours of work per day of employee asparameter
+ 2 -'AddSal()'   which   adds   $10   to   salary   of   the   employee   if   it   is   less   than   $500.
+ 3 - 'AddWork()' which adds $5 to salary of employee if the number of hours of work perday is more than 6 hours.
+*/
+class Employee{
+    private:
+        string name ;
+        string id;
+        int NumberOfHour;
+        double Salary;
+    public:
+        void get_inf(string &a,string &b,int &c);
+        void print_inf();
+        void Add_Salary();
+        void Add_Work();
+        double print_Salary();
+};
+void Employee::get_inf(string &a,string &b,int &c){
+    name=a;
+            id=b;
+            NumberOfHour=c;
+            Salary=NumberOfHour*15*30;
+}
+void Employee::print_inf(){
+    cout<<"\n\t______ Output your Information ______";
+            cout<<"\nName :"<<name;
+            cout<<"\nId :"<<id;
+            cout<<"\nNumber of hour :"<<NumberOfHour;
+            cout<<"\nYour Salary :"<<print_Salary()<<"$";
+}
+void Employee::Add_Salary(){
+    if (Salary<500){
+                Salary+=15;
+            }
+}
+void Employee::Add_Work(){
+    if(NumberOfHour>=6){
+                Salary+=5;
+            }
+}
+double Employee::print_Salary(){
+    return Salary;
+}
+int main (){
+    /*
+    cout<<"\n\t_________________Area and Perimeter of Triangle____________________";
+    Triangle obj1;
+    cout<<"\nPerimeter of Triangle is :"<<obj1.Perimeter();
+    cout<<"\nArea of Triangle is :"<<obj1.Area();
+    cout<<"\n\t_________________Gia tri TB(Average)____________________";
+    float a,b,c;
+    cout<<"\nNhap gia tri cac so hang :";
+    cin>>a>>b>>c;
+    Average obj2=Average(a,b,c);
+    obj2.Calculator();
+    obj2.print_result();
+    cout<<"\n\t__________________ComPlex Number___________________";
+    int real,image;
+    cout<<"\nInput value of real and image number :";cin>>real>>image;
+    Complex_Number obj3=Complex_Number(real,image);
+    obj3.print_complex();
+    */
+    
+    cout<<"\n\t__________________Employee___________________";
+    string name,id;
+    int hour;
+    cout<<"\nNhap ten cua ban :";
+    getline(cin,name);
+    cout<<"\nId :";
+    getline(cin,id);
+    cin.ignore();
+    cout<<"\nSo gio lam trong mot ngay :";
+    cin>>hour;
+    Employee obj4;
+    obj4.get_inf(name,id,hour);
+    obj4.print_inf();
+
+
+    return 0;
+}
--- Cplusplus/Object_Oriented/exercise1.cpp
+++ Cplusplus/Object_Oriented/exercise1.cpp
@@ -1,6 +1,7 @@
 //Write a complete OOP program to represent the student class,define 5 data
 //members ,with constructor ooverloading ,define five student objects and 
 //use friend function to find the number of passing students
+//Object & Oriented includes : Polymorphím ,Abstraction,Inheritance,Overloading,Encapsulation,Friend Function
 #include <iostream>
 #include<string>
 using namespace std;
@@ -10,7 +11,18 @@ class Student {
         string address;
         char mssv[20];
         int age;
+        float dT,dL,dH;
+        float dTB;
     public:
+        Student(float a,float b,float c){
+            dT=a;
+            dL=b;
+            dH=c;
+        }
+        Student(){
+            cout<<"\nNhap diem Toan Li Hoa :";cin>>dT>>dL>>dH;
+            cin.ignore();
+        }
         void get_inf(){
             cout<<"\n\t_____Input your information_____";
             cout<<"\nEnter your name :";getline(cin,name);
@@ -28,12 +40,18 @@ class Student {
             cout<<"\nMy address :"<<address;
             cout<<"\nMy ID :"<<mssv;
             cout<<"\nMy age :"<<age;
+            cout<<"\nDiem Toan Li Hoa :"<<dT<<" "<<dL<<" "<<dH;
+            cout<<"\nDiem trung binh :"<<TB();
+        }
+        float TB(){
+            return (dT+dL+dH)/3;
         }
 };
 
 int main(){
     Student peo1;
     peo1.get_inf();
+    cout<<"\n\t______Information of Student______";
     peo1.display_inf();
     return 0;
 }
